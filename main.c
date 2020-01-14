20a9fc4b4874490cdc81799f2ccbdbdb3dfd20be
Ja <Ja>
2020-01-14 16:49:45 +0100

    Created resized text entry widget

--- main.c
+++ main.c
@@ -8,23 +8,24 @@ static void enter_callback( GtkWidget *widget,GtkWidget *entry )
   entry_text = gtk_entry_get_text (GTK_ENTRY (entry));
   printf ("Entry contents: %s\n", entry_text);
 }
+
 void EntryTextBox(GtkWidget* window)
 {
     GtkWidget* entry;
-    GtkWidget* box;
+    GtkWidget* grid;
     gint tmp_pos;
-    box=gtk_box_new(FALSE,0);
-    gtk_container_add(GTK_CONTAINER(window),box);
-    gtk_widget_show(box);
+    grid=gtk_grid_new();
     entry = gtk_entry_new ();
-    gtk_entry_set_max_length (GTK_ENTRY (entry), 50);
+    gtk_entry_set_max_length (GTK_ENTRY (entry), 30);
     g_signal_connect (entry, "activate",G_CALLBACK (enter_callback),entry);
     gtk_entry_set_text (GTK_ENTRY (entry), "hello");
     tmp_pos = gtk_entry_get_text_length(GTK_ENTRY(entry));
     gtk_editable_insert_text (GTK_EDITABLE (entry), " world", -1, &tmp_pos);
     gtk_editable_select_region (GTK_EDITABLE (entry),0, gtk_entry_get_text_length(GTK_ENTRY(entry)));
-    gtk_box_pack_start (GTK_BOX (box), entry, TRUE, TRUE, 0);
+    gtk_grid_attach(GTK_GRID(grid),entry,0,0,1,1);
+    gtk_container_add(GTK_CONTAINER(window),grid);
     gtk_widget_show (entry);
+    gtk_widget_show(grid);
 }
 void MakeWindow()
 {
@@ -40,7 +41,6 @@ void MakeWindow()
 
 int main(int argc,char* argv[])
 {
-   
     gtk_init(&argc,&argv);
     MakeWindow();
     gtk_main();
