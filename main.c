537e1c8dc481ceb22fd224a00e898fcafbfa1a8e
latarniakonto <43448231+latarniakonto@users.noreply.github.com>
2020-01-17 11:53:29 +0100

    Standing_Controller

--- main.c
+++ main.c
@@ -1,48 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <gtk/gtk.h>
 
-static void enter_callback( GtkWidget *widget,GtkWidget *entry )
+//do h
+struct stand
 {
+    char nazwa[256];
+    char value[256];
+    GtkWidget* textentry[2];
+};
+struct list
+{
+    struct stand stand;
+    struct list* next;
+};
+struct lista* lista;
+
+static void Enter_Callback_Name( GtkWidget *widget,gpointer entry)
+{
+  struct stand* wsk=entry;
   const gchar *entry_text;
-  entry_text = gtk_entry_get_text (GTK_ENTRY (entry));
+  entry_text = gtk_entry_get_text (GTK_ENTRY (wsk->textentry[0]));
+  strcpy(wsk->nazwa,entry_text);
   printf ("Entry contents: %s\n", entry_text);
 }
-
-void EntryTextBox(GtkWidget* window)
+static void Enter_Callback_Value( GtkWidget *widget,gpointer entry)
 {
-    GtkWidget* entry;
-    GtkWidget* grid;
-    gint tmp_pos;
-    grid=gtk_grid_new();
-    entry = gtk_entry_new ();
-    gtk_entry_set_max_length (GTK_ENTRY (entry), 30);
-    g_signal_connect (entry, "activate",G_CALLBACK (enter_callback),entry);
-    gtk_entry_set_text (GTK_ENTRY (entry), "hello");
-    tmp_pos = gtk_entry_get_text_length(GTK_ENTRY(entry));
-    gtk_editable_insert_text (GTK_EDITABLE (entry), " world", -1, &tmp_pos);
-    gtk_editable_select_region (GTK_EDITABLE (entry),0, gtk_entry_get_text_length(GTK_ENTRY(entry)));
-    gtk_grid_attach(GTK_GRID(grid),entry,0,0,1,1);
-    gtk_container_add(GTK_CONTAINER(window),grid);
-    gtk_widget_show (entry);
-    gtk_widget_show(grid);
+  struct stand* wsk=entry;
+  const gchar* entry_text;
+  entry_text = gtk_entry_get_text (GTK_ENTRY (wsk->textentry[1]));
+  strcpy(wsk->value,entry_text);
+  printf ("Entry contents: %s\n", entry_text);
 }
-void MakeWindow()
+struct stand  Make_New_Stand()
 {
-    GtkWidget* window;
-    window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window),"Expensometer");
-    gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 450);
-    g_signal_connect(G_OBJECT(window),"destroy",G_CALLBACK(gtk_main_quit),NULL);
-    EntryTextBox(window);
-    gtk_widget_show(window);
+    struct stand new;
+    new.textentry[0]=gtk_entry_new();
+    new.textentry[1]=gtk_entry_new();
+    gtk_entry_set_max_length(GTK_ENTRY(new.textentry[0]),30);
+    gtk_entry_set_max_length(GTK_ENTRY(new.textentry[1]),30);
+    g_signal_connect (new.textentry[0], "activate",G_CALLBACK (Enter_Callback_Name),&new);
+    g_signal_connect (new.textentry[1], "activate",G_CALLBACK (Enter_Callback_Value),&new);
+    gtk_editable_select_region (GTK_EDITABLE (new.textentry[0]),0, gtk_entry_get_text_length(GTK_ENTRY(new.textentry[0])));
+    gtk_editable_select_region (GTK_EDITABLE (new.textentry[1]),0, gtk_entry_get_text_length(GTK_ENTRY(new.textentry[1])));
+    return stand;
 }
-
-int main(int argc,char* argv[])
+void Add(struct list* list)
 {
-    gtk_init(&argc,&argv);
-    MakeWindow();
-    gtk_main();
+    if(list==NULL)
+    {
+        list->stand=Make_New_Stand();
+    }else
+    {
+        while(list->next!=NULL)
+        {
+            list=list->next;
+        }
+        struct stand new_stand=Make_New_Stand();
+        struct lista* new_list=(struct list*)calloc(1,sizeof(struct list));
+        new_list->stand=new_stand;
+        new_list->next=NULL;
+    }
+}
+int main()
+{
+
     return 0;
 }
